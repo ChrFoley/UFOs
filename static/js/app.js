0212ea8f1c09ea0ec90d283d01bc4d62a4a64477
Chris foley <chris.w.foley1980@gmail.com>
2020-08-19 18:37:15 -0500

    final outputs

--- static/js/app.js
+++ static/js/app.js
@@ -23,3 +23,64 @@ function buildTable(data) {
       );
     });
   }
+
+  var filters = {};
+
+    function updateFilters() {
+
+    let changedElement = d3.select(this);
+    let elementValue = changedElement.property("value");
+    let filterId = changedElement.attr("id");
+  
+    if (elementValue) {
+      filters[filterId] = elementValue;
+    }
+    else {
+      delete filters[filterId];
+    }
+  
+    filterTable();
+    
+  }
+  
+  function filterTable() {
+  
+    let filteredData = tableData;
+  
+    Object.entries(filters).forEach(([key, value]) => {
+      filteredData = filteredData.filter(row => row[key] === value);
+    });
+  
+    buildTable(filteredData);
+  }
+  
+  d3.selectAll("input").on("change", updateFilters);
+  
+  buildTable(tableData);
+
+
+  // Original function...
+//   function handleClick() {
+//     // Grab the datetime value from the filter
+//     let date = d3.select("#datetime").property("value");
+//     let filteredData = tableData;
+    
+//      // Check to see if a date was entered and filter the
+//     // data using that date.
+//     if (date) {
+//       // Apply `filter` to the table data to only keep the
+//       // rows where the `datetime` value matches the filter value
+//       filteredData = filteredData.filter(row => row.datetime === date);
+//     }
+    
+//      // Rebuild the table using the filtered data
+//     // @NOTE: If no date was entered, then filteredData will
+//     // just be the original tableData.
+//     buildTable(filteredData);
+//   }
+  
+//   // Attach an event to listen for the form button
+//   d3.selectAll("#filter-btn").on("click", handleClick);
+  
+//   // Build the table when the page loads
+//   buildTable(tableData);
